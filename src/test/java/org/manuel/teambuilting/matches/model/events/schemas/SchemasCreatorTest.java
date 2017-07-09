b979820943f4ebb301811161c10e86a35612d372
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-07-09 22:33:30 +0200

    TeamBuiltingMatches: Adding gradle wrapper

--- src/test/java/org/manuel/teambuilting/matches/model/events/schemas/SchemasCreatorTest.java
+++ src/test/java/org/manuel/teambuilting/matches/model/events/schemas/SchemasCreatorTest.java
@@ -2,16 +2,7 @@ package org.manuel.teambuilting.matches.model.events.schemas;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
-import org.manuel.teambuilting.matches.model.Match;
-import org.manuel.teambuilting.matches.model.MatchImpl;
-import org.manuel.teambuilting.matches.model.TeamInMatch;
-import org.manuel.teambuilting.matches.model.parts.MatchPart;
-import org.manuel.teambuilting.matches.model.team.RegisteredTeamInfo;
-import org.manuel.teambuilting.matches.model.team.TeamInfo;
-import org.manuel.teambuilting.matches.model.team.UnRegisteredTeamInfo;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.manuel.teambuilting.matches.model.events.GoalEvent;
 
 /**
  * @author Manuel Doncel Martos
@@ -21,17 +12,7 @@ public class SchemasCreatorTest {
 
     @Test
     public void testSerializeGoalEvent() throws JsonProcessingException {
-        final SchemasCreator schemasCreator = new SchemasCreator();
-
-        final TeamInfo homeTeamInfo = new RegisteredTeamInfo("homeTeamInfoId", "homeTeamId");
-        final TeamInMatch homeTeam = TeamInMatch.builder().teamInfo(homeTeamInfo).selectedPlayers(new ArrayList<>()).build();
-
-        final TeamInfo awayTeamInfo = new UnRegisteredTeamInfo("awayTeamInfoId", "Away Team Name");
-        final TeamInMatch awayTeam = TeamInMatch.builder().teamInfo(awayTeamInfo).selectedPlayers(new ArrayList<>()).build();
-
-        List<MatchPart> parts = new ArrayList<>();
-        final Match match = new MatchImpl(null, homeTeam, awayTeam, null, parts, null, new ArrayList<>());
-        schemasCreator.createGoalEventSchema();
+        SchemasCreator.createSchemaFor(GoalEvent.class);
     }
 
 }
