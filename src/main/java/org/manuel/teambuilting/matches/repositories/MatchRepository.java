63ed589c3201624a041dfa6632b4f27af4d16d55
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-08-28 16:51:36 +0200

    TeambuiltingMatches: Adding filter matches per player played

--- src/main/java/org/manuel/teambuilting/matches/repositories/MatchRepository.java
+++ src/main/java/org/manuel/teambuilting/matches/repositories/MatchRepository.java
@@ -24,6 +24,7 @@ public interface MatchRepository extends MongoRepository<Match, String> {
      */
     Set<Match> findByMatchPartsStartingTimeIsBetween(Date from, Date to);
 
+    // TODO - try to join them because they do the same
     Page<Match> getByMatchPartsStartingTimeIsBetween(Date from, Date to, Pageable pageable);
 
     Page<Match> findByIdIn(Pageable pageable, Set<String> ids);
